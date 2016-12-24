994fc9a0cfba29209e215fd9b4415a48fd6eb7f2
Florent Destremau <florent.destremau@gmail.com>
2016-12-24 14:44:00 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+const API_URL = 'http://localhost:3001';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      companies: [],
+    };
+  }
+  
+  componentDidMount() {
+    axios.get(`${API_URL}/companies`)
+    .then(response => this.setState({
+      companies: response.data,
+    }))
+    .catch(error => console.error(error));
+  }
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="jumbotron App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>
+            Bienvenue sur
+            <br></br>
+            <em>Voler avec mon boardbag !</em>
+          </h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2 className="App-intro">
+          Voici la liste des companies aériennes référencées :
+        </h2>
+        {
+          this.state.companies.length > 0 ? (
+            <ul className="list-group">
+              {this.state.companies.map(company => (
+              <li key={company.id} className="list-group-item text-capitalize">
+                {company.name}
+                {company.free ?
+                  (<span className="badge">Gratuit</span>)
+                  : null
+                }
+                {company.golfbag ?
+                  (<span className="badge">Golfbag</span>)
+                  : null
+                }
+              </li>
+            ))}
+            </ul>) : <p><em>Il n'y a pas de companies référencées</em></p>
+        }
       </div>
     );
   }
